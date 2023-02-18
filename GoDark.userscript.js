07a6050fb50282fe9834bde55772b99a46444357
epsluder <82414253+epsluder@users.noreply.github.com>
2023-02-18 17:42:00 -0500

    Update GoDark.userscript.js

--- GoDark.userscript.js
+++ GoDark.userscript.js
@@ -3,7 +3,7 @@
 // @namespace https://github.com/epsluder/grace-sermons-dark
 // @match https://notes.subsplash.com/*
 // @description Change the CSS of Sermon Notes to Dark Mode
-// @version 1.0
+// @version 1.01
 // ==/UserScript==
 
-alert("this is working");
+document.querySelector("body").style("background-color: black !important");
