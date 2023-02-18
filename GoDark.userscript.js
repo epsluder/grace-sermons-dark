559745ce86288289265c9caea971240dad431167
epsluder <82414253+epsluder@users.noreply.github.com>
2023-02-18 17:34:57 -0500

    Create GoDark.userscript.js

---
+++ GoDark.userscript.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name Grace Sermon Dark Mode
+// @namespace https://github.com/epsluder/grace-sermons-dark
+// @match https://notes.subsplash.com/*
+// @description Change the CSS of Sermon Notes to Dark Mode
+// @version 1.0
+// ==/UserScript==
+
+alert("this is working");
